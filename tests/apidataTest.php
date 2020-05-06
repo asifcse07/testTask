804838fa500c0fde9709433a6ec2f217fddcfa56
asif.cse07 <asif.cse07@gmail.com>
2020-05-07 02:34:25 +0600

    code push 2

--- tests/apidataTest.php
+++ tests/apidataTest.php
@@ -8,8 +8,8 @@ class apidataTest extends \PHPUNIT_Framework_TestCase{
     public function testGetBinData(){
         try {
             // something here
-            $value[] = "";//"45717360";
-            $data = $this->assertTrue(true);
+            $value[] = "45717360";
+            $data = $this->assertNotEmpty($value);
             return $data;
         } catch (\SpecificException $e) {
             // force a fail:
@@ -21,7 +21,7 @@ class apidataTest extends \PHPUNIT_Framework_TestCase{
     public function testGetRate(){
         try {
             $value[2] = "EUR";
-            $this->assertTrue(False);
+            $data = $this->assertNotEmpty($value);
         } catch (\SpecificException $e) {
             // force a fail:
             throw new \PHPUnitException("This was not expected.");
